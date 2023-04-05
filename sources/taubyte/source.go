efc27ac53465abb7a6bdd6a27fc283b794302dd9
Tafseer Khan <tafseer.k.khan@gmail.com>
2023-04-05 14:37:19 -0500

    added source test

--- sources/taubyte/source.go
+++ sources/taubyte/source.go
@@ -8,7 +8,6 @@ import (
 	module "github.com/taubyte/vm/modules/source"
 )
 
-// TODO: add caching .. or maybe use mmap
 type source struct {
 	loader vm.Loader
 }
@@ -24,13 +23,13 @@ func New(loader vm.Loader) vm.Source {
 func (s *source) Module(ctx vm.Context, name string) (vm.SourceModule, error) {
 	_reader, err := s.loader.Load(ctx, name)
 	if err != nil {
-		return nil, fmt.Errorf("Loading module `%s` failed with %w", name, err)
+		return nil, fmt.Errorf("loading module `%s` failed with %w", name, err)
 	}
 
 	_source, err := io.ReadAll(_reader)
 	_reader.Close()
 	if err != nil {
-		return nil, fmt.Errorf("Reading module `%s` failed with %w", name, err)
+		return nil, fmt.Errorf("reading module `%s` failed with %w", name, err)
 	}
 
 	return module.New(_source)
